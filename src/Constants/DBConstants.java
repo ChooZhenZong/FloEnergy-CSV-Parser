24f7df788f84adc06a7323f667a2b57c312080c2
zzz zz <ChooZhenZong@outlook.com>
2026-04-02 19:47:13 +0800

    First initial commit

---
+++ src/Constants/DBConstants.java
@@ -0,0 +1,9 @@
+package Constants;
+
+public class DBConstants {
+
+    public static final String METER_READING = "meter_readings";
+    public static final String NMI = "NMI";
+    public static final String TIMESTAMP = "timestamp";
+    public static final String CONSUMPTION = "consumption";
+}
